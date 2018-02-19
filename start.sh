ffd232d7895292dbfb2c1b91ffd5679944fdedcb
ojcarrillo <oj_carrillo@yahoo.com>
2018-02-19 11:42:05 +0000

    ajuste red del contenedor

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 # ejecuta el container de rabbitmq
 docker run \
 --name dk_rabbitmq \
+--net=backend \
 -d -p 5672:5672 -p 15672:15672 \
 --hostname my-rabbit \
 -v /volume/docker/rabbitmq/data/log:/data/log \
